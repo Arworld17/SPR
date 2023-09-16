ab41a4ad9db21dcc2706f1ce39e4bc1632153d30
Arworld17 <144979815+Arworld17@users.noreply.github.com>
2023-09-16 09:16:02 +0800

    Add files via upload

--- 随机排序2.c
+++ 随机排序2.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <time.h>
+#include <stdlib.h> //srand，qsort 
+#include <string.h>//strcpy 
+#include <time.h>//time 
 
-typedef struct {
+typedef struct {           //别名修饰结构体 
     int snowboard;
     char name[10];
 } Person;
 
-void shuffleArray(int arr[], int n) {
-    srand(time(0));
+void shuffleArray(int arr[], int n) {             //定义函数和随机排列 
+    srand(time(0));                               //根据当前时间来设置随机数种子 
 int i;
     for ( i = n - 1; i > 0; i--) {
         int j = rand() % (i + 1);
@@ -19,14 +19,14 @@ int i;
     }
 }
 
-int compare(const void* a, const void* b) {
-    Person* personA = (Person*)a;
+int compare(const void* a, const void* b) {        //定义，进行比较 
+    Person* personA = (Person*)a;               //类型转换 
     Person* personB = (Person*)b;
     return personA->snowboard - personB->snowboard;
 }
 
-int main() {
-    int snowboards[] = {1, 2, 3, 4};
+int main() { 
+    int snowboards[] = {1, 2, 3, 4};            //赋名 
     Person people[4];
     strcpy(people[0].name, "小刘");
     strcpy(people[1].name, "小周");
@@ -41,7 +41,7 @@ int i;
         people[i].snowboard = snowboards[i];
     }
 
-    qsort(people, 4, sizeof(Person), compare);
+    qsort(people, 4, sizeof(Person), compare);               //比较与排序 
 
     for ( i = 0; i < 4; i++) {
         printf("滑雪板 %d 由 %s 使用\n", people[i].snowboard, people[i].name);
